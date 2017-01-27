4f64c0e45443b3d621e79f5814782c0ac9417b11
matt-winzer <matt.winzer@gmail.com>
2017-01-27 11:36:11 -0700

    created dead_cat migration

---
+++ migrations/20170127113500_dead_cat.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('dead_cat', function(table){
+    table.increments();
+    table.text('name').notNullable();
+    table.text('color').notNullable();
+    table.text('type').notNullable();
+    table.boolean('is_alive').notNullable().defaultTo(false);
+    table.integer('owner_id').unsigned().references('owner.id').onDelete('cascade');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('dead_cat');
+};
