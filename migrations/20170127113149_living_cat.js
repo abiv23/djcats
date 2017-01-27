a2112d5d7bf348931a47f8e360e5123247d21791
matt-winzer <matt.winzer@gmail.com>
2017-01-27 11:34:41 -0700

    created living_cat migration

---
+++ migrations/20170127113149_living_cat.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('living_cat', function(table){
+    table.increments();
+    table.text('name').notNullable();
+    table.text('color').notNullable();
+    table.text('type').notNullable();
+    table.boolean('is_alive').notNullable().defaultTo(true);
+    table.integer('age').notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('living_cat');
+};
