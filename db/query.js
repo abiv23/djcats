b353e5f198ee660e5be819ec9b1898a7e71409f8
matt-winzer <matt.winzer@gmail.com>
2017-01-27 13:54:48 -0700

    queries and routes for get all dead/living cats complete

--- db/query.js
+++ db/query.js
@@ -3,14 +3,15 @@ var router = express.Router();
 var knex = require('./knex');
 var bcrypt = require('bcrypt');
 
+function getAllDeadCats() {
+  return knex('dead_cat');
+}
+
+function getAllLivingCats() {
+  return knex('living_cat');
+}
 
 module.exports = {
-  getAllUsers,
-  getUserByEmail,
-  createUser,
-  getAllExercises,
-  createExercise,
-  getExerciseById,
-  editExercise,
-  deleteExercise
+  getAllDeadCats,
+  getAllLivingCats
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var query = require('../db/query');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/memorial', (req, res, next) => {
+  query.getAllDeadCats().then(cats => {
+    res.json(cats);
+  });
+});
+
+router.get('/happiness', (req, res, next) => {
+  query.getAllLivingCats().then(cats => {
+    res.json(cats);
+  });
+});
+
 
 module.exports = router;
